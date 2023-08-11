b4358e907b60f05cd63e9c76d5af7b4a3ae6355b
Ana Segovia <anasegovia505@gmail.com>
2023-08-11 14:39:39 -0500

    js updating score board

--- scoreRoutes.js
+++ scoreRoutes.js
@@ -3,33 +3,53 @@ var ctx = canvas.getContext("");
 var score = 0
 
 
-function drawScore() {
-  ctx.font = "16px Arial";
-  ctx.fillStyle = "#0095DD";
-  ctx.fillText(`Score: ${score}`, 8, 20);
+var p1Btn = document.getElementById("p1");
+var p2Btn = document.getElementById("p2");
+var resetBtn = document.getElementById("reset");
+var p1Display = document.getElementById("p1Display");
+var p2Display = document.getElementById("p2Display");
+var numInput = document.querySelector("input[type=number]");
+var winningScoreDisplay = document.querySelector("p span");
+
+var p1Score = 0;
+var p2Score = 0;
+var gameOver = false;
+var winningScore = 5;
+
+function reset() {
+    p1Score = p1Display.textContent = p2Score = p2Display.textContent = 0;
+    p1Display.classList.remove("winner");
+    p2Display.classList.remove("winner");
+    gameOver = false;
 }
 
-
-//adding points to the score board
-function collisionDetection() {
-  for (let c = 0; c < brickColumnCount; c++) {
-    for (let r = 0; r < brickRowCount; r++) {
-      const b = bricks[c][r];
-      if (b.status === 1) {
-        if (
-          x > b.x &&
-          x < b.x + brickWidth &&
-          y > b.y &&
-          y < b.y + brickHeight
-        ) {
-          dy = -dy;
-          b.status = 0;
-          score++;
+p1Btn.addEventListener("click", function(){
+    if(!gameOver) {
+        p1Score++;
+        if(p1Score === winningScore) {
+            gameOver = true;
+            p1Display.classList.add("winner");
         }
-      }
+        p1Display.textContent = p1Score;
     }
-  }
-}
+});
+p2Btn.addEventListener("click", function(){
+    if(!gameOver) {
+        p2Score++;
+        if(p2Score === winningScore) {
+            gameOver = true;
+            p2Display.classList.add("winner");
+        }
+        p2Display.textContent = p2Score;
+    }
+});
+resetBtn.addEventListener("click", function() {
+    reset();
+});
+numInput.addEventListener("change", function() {
+    winningScore = winningScoreDisplay.textContent = Number(this.value);
+    reset();
+});
 
 //Displays wining message
 function collisionDetection() {
