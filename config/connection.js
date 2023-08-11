f9ffe9c912564a6eb81ceb408835763d3fa4ea49
Griffin <liq543@gmail.com>
2023-08-11 15:53:50 -0500

    fix again

--- config/connection.js
+++ config/connection.js
@@ -2,9 +2,9 @@ const Sequelize = require('sequelize');
 require('dotenv').config();  // This npm package helps you to use environment variables.
 
 let sequelize;
-
+let dev = 1;
 // If you have the DATABASE_URL env variable (Heroku + ClearDB), use that
-if (process.env.DB_DATABASE_URL) {
+if (dev = 1) {
   sequelize = new Sequelize(process.env.DB_DATABASE_URL, {
     dialect: 'mysql',
     logging: true, // Optional: Set to false if you dont want to see logs
