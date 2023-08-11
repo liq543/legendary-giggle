d8fa3f7736e06a3286cf8bf7625bb1ae7fca569d
Griffin <liq543@gmail.com>
2023-08-11 15:46:40 -0500

    heroku database linkage maybe idk

--- config/connection.js
+++ config/connection.js
@@ -1,12 +1,21 @@
-// Importing necessary npm packages
 const Sequelize = require('sequelize');
 require('dotenv').config();  // This npm package helps you to use environment variables.
 
-// Setting up the connection to the MySQL database
-const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASSWORD, {
-  host: 'localhost',
-  dialect: 'mysql',
-  port: 3306  // Default MySQL port
-});
+let sequelize;
+
+// If you have the DATABASE_URL env variable (Heroku + ClearDB), use that
+if (process.env.DATABASE_URL) {
+  sequelize = new Sequelize(process.env.DATABASE_URL, {
+    dialect: 'mysql',
+    logging: false, // Optional: Set to true if you want to see SQL queries in the logs
+  });
+} else {
+  // Otherwise, use local .env configurations
+  sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASSWORD, {
+    host: 'localhost',
+    dialect: 'mysql',
+    port: 3306  // Default MySQL port
+  });
+}
 
 module.exports = sequelize;
