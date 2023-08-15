c6722667a55e02913fe164a6c7e7f69c4ad76eec
Griffin <liq543@gmail.com>
2023-08-14 21:56:00 -0500

    Host is now properly assigned to the first player joining the room

--- serverjs/socketLogic.js
+++ serverjs/socketLogic.js
@@ -14,7 +14,7 @@ function initializeSocket(server) {
 
     io.on('connection', (socket) => {
         console.log('User connected to socket: ', socket.id);
-
+        
         socket.on('createRoom', () => {
             // If this socket was previously the host of another room, delete that room
             for (let existingRoomCode in rooms) {
@@ -25,35 +25,39 @@ function initializeSocket(server) {
                     break; // Assuming a socket can only be a host of one room at a time
                 }
             }
-        
+            
             const roomCode = Math.random().toString(36).substring(7).toUpperCase(); // Generate random room code
-            rooms[roomCode] = { host: socket.id, guest: null };
-            socket.emit('roomCreated', roomCode); // Notify the host about the room creation
-
-            // Assign the role of "host" to the socket / user who created the room
-            socket.emit('assignRole', 'host');
+            rooms[roomCode] = { host: null, guest: null }; // Do not set the host when the room is created
+            socket.emit('roomCreated', roomCode); // Notify the user about the room creation
         });
-
+        
         socket.on('joinRoom', (roomCode) => {
+            console.log(`User with socket ID ${socket.id} attempting to join room: ${roomCode}`);
             if (rooms[roomCode]) {
-                if (rooms[roomCode].host === socket.id) {
-                    // The host is joining the room they created
+                if (!rooms[roomCode].host) {
+                    // First person to join gets the host role
+                    rooms[roomCode].host = socket.id;
                     socket.join(roomCode);
                     socket.emit('joinedRoom', roomCode);
+                    socket.emit('assignRole', 'host');
+                    console.log(`User with socket ID ${socket.id} assigned as host for room: ${roomCode}`);
                 } else if (!rooms[roomCode].guest) {
-                    // A guest is joining the room
+                    // Second person to join gets the guest role
                     rooms[roomCode].guest = socket.id;
                     socket.join(roomCode);
                     socket.emit('joinedRoom', roomCode);
                     io.to(roomCode).emit('playersReady');
+                    socket.emit('assignRole', 'guest');
+                    console.log(`User with socket ID ${socket.id} assigned as guest for room: ${roomCode}`);
                 } else {
+                    console.log(`User with socket ID ${socket.id} attempted to join room: ${roomCode}, but it's full.`);
                     socket.emit('error', 'Room is full.');
                 }
             } else {
+                console.log(`User with socket ID ${socket.id} attempted to join with an invalid room code: ${roomCode}`);
                 socket.emit('error', 'Invalid room code.');
             }
         });
-
         socket.on('pressButton', (roomCode) => {
             socket.to(roomCode).emit('otherUserPressed');
             console.log(`sending ${roomCode} to other user`); // Corrected string interpolation
